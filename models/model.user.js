f19da460feafc9f6c8ca507ea2b5d8401de49bfd
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-04-22 06:13:58 +0100

    User route

--- models/model.user.js
+++ models/model.user.js
@@ -1,10 +1,11 @@
  
 /**
- * Model for user and admin
+ * Model for user 
  */
 const sequelize = require('./../database');
 const {DataTypes, Model} = require('sequelize');
 const tableName = " reach_users";
+const queryInterface = sequelize.getQueryInterface();
 /**
  * Model extending sequelize model class
  */
@@ -18,10 +19,19 @@ ModelUser.init({
     fullname: {type: DataTypes.STRING, allowNull: false},
     phone: {type: DataTypes.STRING, allowNull: true},
     blocked: {type: DataTypes.BOOLEAN, defaultValue: false},
+    lastLogin: {type: DataTypes.STRING},
     token: {type: DataTypes.STRING, allowNull: true, unique: true},
 }, {sequelize, tableName});
 /**
  * Run belonging and relationship before sync()
  */
+
+// queryInterface.addColumn(tableName, 'lastLogin', {
+//     type: DataTypes.STRING
+// });
+
+// queryInterface.removeColumn('reach_users', 'trxTrials');
+
+
 sequelize.sync();
 module.exports = ModelUser;
